cfb29068893590ac82609c03b4b910accc5b75c4
AnderGI02 <ander.gutierrez.ibarguen@zabalburu.org>
2024-02-29 13:15:33 +0100

    RabbitMQ producer and consumer

--- app/scrapperms/Infraestructure/Controllers/CarsGetController.js
+++ app/scrapperms/Infraestructure/Controllers/CarsGetController.js
@@ -1,10 +1,60 @@
 import { ObtenerCochesScrapeados } from "../../Application/ObtenerCochesScrappeadosUseCase/ObtenerCochesScrappeados.js";
 import { PuppeteerScrapper } from "../ScrapperImplementations/PuppeteerScraper.js";
+import amqp from "amqplib";
 
 const pupperterScrapper = PuppeteerScrapper();
 const implementations = [pupperterScrapper];
 
+const queue = "car_scrapping";
+
 export const getAllCars = async (req, res) => {
   const data = await ObtenerCochesScrapeados(implementations);
+  (async () => {
+    let connection;
+    try {
+      connection = await amqp.connect("amqp://localhost");
+      const channel = await connection.createChannel();
+
+      await channel.assertQueue(queue, { durable: false });
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
+      console.log(" [x] Sent '%s'", JSON.stringify(data));
+      await channel.close();
+    } catch (err) {
+      console.warn(err);
+    } finally {
+      if (connection) await connection.close();
+    }
+  })();
+
   res.json(data);
+
+  (async () => {
+    try {
+      const connection = await amqp.connect("amqp://localhost");
+      const channel = await connection.createChannel();
+
+      process.once("SIGINT", async () => {
+        await channel.close();
+        await connection.close();
+      });
+
+      await channel.assertQueue(queue, { durable: false });
+      await channel.consume(
+        queue,
+        (message) => {
+          if (message) {
+            console.log(
+              " [x] Received '%s'",
+              JSON.stringify(JSON.parse(message.content.toString()))
+            );
+          }
+        },
+        { noAck: true }
+      );
+
+      console.log(" [*] Waiting for messages. To exit press CTRL+C");
+    } catch (err) {
+      console.warn(err);
+    }
+  })();
 };
