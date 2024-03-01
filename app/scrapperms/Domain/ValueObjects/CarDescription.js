fee492cc99f2126f63107740320e1931f8aef47d
AnderGI02 <ander.gutierrez.ibarguen@zabalburu.org>
2024-03-01 09:46:43 +0100

    Required fild properties validation

--- app/scrapperms/Domain/ValueObjects/CarDescription.js
+++ app/scrapperms/Domain/ValueObjects/CarDescription.js
@@ -2,11 +2,12 @@ import { z } from "zod";
 
 const carDescriptionSchema = z
   .string({
+    required_error: "Description is required",
     invalid_type_error: "Description must be a string",
   })
   .trim()
-  .min(20, { message: "Must be 20 or more characters long" })
-  .default("Car with no description");
+  .min(20, { message: "Must be 20 or more characters long" });
+//.default("Car with no description");
 
 // Validaciones strictas por defecto lanza una excepcción
 export const CarDescription = (description) =>
