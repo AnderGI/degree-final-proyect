d592762bbe74916cd1ef7a984d1bd77486a2161a
AnderGI02 <ander.gutierrez.ibarguen@zabalburu.org>
2024-03-05 08:43:55 +0100

    Car VO added

--- app/cochesms/src/main/java/com/example/coches/Domain/Entities/Car.java
+++ app/cochesms/src/main/java/com/example/coches/Domain/Entities/Car.java
@@ -4,24 +4,29 @@ import com.example.coches.Domain.ValueObjects.CarBrand;
 import com.example.coches.Domain.ValueObjects.CarDescription;
 import com.example.coches.Domain.ValueObjects.CarPrice;
 import com.example.coches.Domain.ValueObjects.CarTitle;
+import com.example.coches.Domain.ValueObjects.CarUrl;
 
 public class Car {
 	private CarTitle title;
 	private CarDescription description;
 	private CarBrand brand;
-	private String carImageURL;
-	private String carAnnouncement;
+	private CarUrl carImageURL;
+	private CarUrl carAnnouncementUrl;
 	private CarPrice price;
 	
 	public Car(
 			String title, 
 			String description,
 			String brand,
-			Double precio) {
+			Double precio,
+			String carImageUrl,
+			String carAnnouncmentURL) {
 		this.title = new CarTitle(title);
 		this.description = new CarDescription(description);
 		this.brand = new CarBrand(brand);
 		this.price = new CarPrice(precio);
+		this.carImageURL = carImageURL;
+		this.carAnnouncementUrl = carAnnouncementUrl;
 	}
 	
 	public String getTitleValue() {
@@ -40,6 +45,15 @@ public class Car {
 		return this.brand.getValue();
 	}	
 	
+
+	public String getCariMAGEURLValue() {
+		return this.carImageURL.getValue();
+	}
+	
+	public String getCarAnnouncmentURLValue() {
+		return this.carAnnouncementUrl.getValue();
+	}
+	
 	public void updateTitle(String title) {
 		this.title = new CarTitle(title);
 	}
@@ -47,4 +61,9 @@ public class Car {
 	public void updateDescription(String description) {
 		this.description = new CarDescription(description);
 	}
+	
+	public void updateCarImageUrl(String url) {
+		this.carImageURL = new CarUrl(url);
+	}
+	
 }
