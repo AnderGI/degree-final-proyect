1f98e3011f7849407f8da9d7946dd1f6e43312b0
AnderGI <agibarguen@gmail.com>
2024-03-16 19:41:20 +0100

    Add car and check for added car equality test added

--- app/cochesms/src/test/java/com/example/coches/cars/infraestructure/adapters/cars/CarRepositoryTest.java
+++ app/cochesms/src/test/java/com/example/coches/cars/infraestructure/adapters/cars/CarRepositoryTest.java
@@ -31,12 +31,14 @@ public class CarRepositoryTest {
 	@Autowired
 	private CarRepository repository;
 
+	// Get cars
 	@Test
 	void it_should_retreive_all_cars_as_a_list() {
 		assertNotNull(repository.getCars());
 		assertEquals(repository.getCars().getClass(), ArrayList.class);
 	}
-
+		
+	// Get car by id
 	@Test
 	void it_should_retrieve_a_existing_car_by_its_id() {
 		Car firstCar = repository.getCars().get(0);
@@ -53,12 +55,40 @@ public class CarRepositoryTest {
 		assertEquals(firstCar.getCarAnnouncmentURLValue(), retrievedFirstCar.getCarAnnouncmentURLValue());
 	}
 
+	// Get car by non existing id --> null
 	@Test
 	void it_should_retrieve_null_if_car_id_does_not_exists() {
 		Car nullableCar = repository.getCar("aaaaa");
 		assertEquals(nullableCar, null);
 	}
 
+	// Save car
+	@Test
+	void it_should_save_a_car_and_check_for_car_equality() {
+		// Crear los objetos necesarios para pasar al constructor de Car
+		CarTitle title = new CarTitle("Ford Mustang GT");
+		CarDescription description = new CarDescription("Potente y deportivo");
+		CarBrand brand = new CarBrand("Ford");
+		CarPrice price = new CarPrice(45000.0);
+		CarUrl carImageURL = new CarUrl("https://example.com/ford-mustang-gt.jpg");
+		CarUrl carAnnouncementUrl = new CarUrl("https://example.com/ford-mustang-gt-listing");
+
+		// Crear el objeto Car usando el constructor
+		Car car = new Car(title, description, brand, price, carImageURL, carAnnouncementUrl);
+		Car addedCar = repository.addCar(car);
+		assertNotNull(addedCar);
+		assertNotNull(addedCar.getIdValue());
+		// Todos los campos iguales
+		assertEquals(addedCar.getIdValue(), car.getIdValue());
+		assertEquals(addedCar.getTitleValue(), car.getTitleValue());
+		assertEquals(addedCar.getDescriptionValue(), car.getDescriptionValue());
+		assertEquals(addedCar.getBrandValue(), car.getBrandValue());
+		assertEquals(addedCar.getCarPriceValue(), car.getCarPriceValue());
+		assertEquals(addedCar.getCarImageUrlValue(), car.getCarImageUrlValue());
+		assertEquals(addedCar.getCarAnnouncmentURLValue(), car.getCarAnnouncmentURLValue());
+		
+	}
+	
 	/*
 	 * void it_should_save_a_new_car_with_all_values() { Car newCar = new Car(new
 	 * CarTitle("Coche 2"), new CarDescription("Descripcción del coche 2"), new
