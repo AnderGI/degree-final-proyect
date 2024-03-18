7c0e2da6f6a0e4094ad3f612debf13df491a151f
AnderGI <agibarguen@gmail.com>
2024-03-18 12:35:58 +0100

    Car get controller mock test added for getting all cars

---
+++ app/cochesms/src/test/java/com/example/coches/cars/application/controllers/CarsGetControllerTest.java
@@ -0,0 +1,100 @@
+package com.example.coches.cars.application.controllers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import com.example.coches.cars.domain.car.Car;
+import com.example.coches.cars.domain.car.CarBrand;
+import com.example.coches.cars.domain.car.CarDescription;
+import com.example.coches.cars.domain.car.CarPrice;
+import com.example.coches.cars.domain.car.CarRepository;
+import com.example.coches.cars.domain.car.CarTitle;
+import com.example.coches.cars.domain.car.CarUrl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// MOCKING -> MOCKITO
+@WebMvcTest
+public class CarsGetControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private CarGetController getController;
+	@Autowired
+	private ObjectMapper mapper;
+	// Mockeamos una instancia del bean CarRepository para no utilizar el mismo
+	@MockBean
+	private CarRepository repository;
+	
+	@Test
+	void it_should_return_empty_array_if_empty_repository() throws Exception{
+		List<Car> expectedCars = new ArrayList<>();
+		// No añadimos nada
+		// Especificamos lo que tiene que hacer cuando se ejecute el metodo
+		when(repository.getCars()).thenReturn(expectedCars);
+		// Realizamos la accion al endpoint especificado
+		mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
+		.andDo(print()) // Mostrar por consola
+		.andExpectAll(
+				// 200 OK
+				status().isOk(),
+				// Devuelve el contenido como un json
+				content().contentType(MediaType.APPLICATION_JSON),
+				// Devuelve una lista vacia
+				content().json(mapper.writeValueAsString(expectedCars))
+			);
+	}
+	
+	@Test
+	void it_should_return_a_list_of_products_when_get() throws Exception {
+        List<Car> expectedCars = Arrays.asList(
+                new Car(new CarTitle("Chevrolet Camaro SS"), new CarDescription("Muscle car americano"), 
+                        new CarBrand("Chevrolet"), new CarPrice(55000.0), 
+                        new CarUrl("https://example.com/chevrolet-camaro-ss.jpg"), 
+                        new CarUrl("https://example.com/chevrolet-camaro-ss-listing")),
+                new Car(new CarTitle("BMW M3"), new CarDescription("Sedán deportivo de lujo"), 
+                        new CarBrand("BMW"), new CarPrice(70000.0), 
+                        new CarUrl("https://example.com/bmw-m3.jpg"), 
+                        new CarUrl("https://example.com/bmw-m3-listing")),
+                new Car(new CarTitle("Audi R8"), new CarDescription("Supercar de alta gama"), 
+                        new CarBrand("Audi"), new CarPrice(150000.0), 
+                        new CarUrl("https://example.com/audi-r8.jpg"), 
+                        new CarUrl("https://example.com/audi-r8-listing"))
+            );
+		repository.addCar(expectedCars.get(0));
+		repository.addCar(expectedCars.get(1));
+		repository.addCar(expectedCars.get(2));
+		
+		// Le especificamos que cuando llame a / devuelva todas las instancias
+		when(repository.getCars()).thenReturn(expectedCars);
+		
+		// Hacemos la operación de get al endpoint
+		mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
+		.andDo(print()) // mostrar por consola
+		.andExpectAll(
+				// Estado correcto 200 OK
+				status().isOk(),
+				// El contenido se envia como json
+				content().contentType(MediaType.APPLICATION_JSON),
+				// El contenido es el mismo a la lista de expectedCars
+				content().json(mapper.writeValueAsString(expectedCars))
+		);
+	}
+	
+	
+}
