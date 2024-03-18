867e1ab6bd86179fa39d584e66a022fe71895974
AnderGI <agibarguen@gmail.com>
2024-03-18 16:40:03 +0100

    Unused classes removed

--- app/cochesms/src/main/java/com/example/coches/cars/domain/car/CarDTOFieldValidator.java
+++
@@ -1,7 +0,0 @@
-package com.example.coches.cars.domain.car;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public interface CarDTOFieldValidator {
-	String validateCarDTOField(CarDTO carDto, ObjectMapper objMapper);
-}
