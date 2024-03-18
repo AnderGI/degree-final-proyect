c103ff6d64d688c2511361f4105b7238e155a085
AnderGI <agibarguen@gmail.com>
2024-03-18 12:55:38 +0100

    Car get controller mock test added for getting a single car

--- app/cochesms/src/main/java/com/example/coches/cars/application/controllers/CarGetController.java
+++ app/cochesms/src/main/java/com/example/coches/cars/application/controllers/CarGetController.java
@@ -3,7 +3,9 @@ package com.example.coches.cars.application.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.coches.cars.domain.car.Car;
@@ -17,11 +19,17 @@ final public class CarGetController {
 	@Autowired
 	private CarRepository repo;
 	
-
-	
 	@GetMapping("/")
 	public List<Car> getCars(){
 		// return 
 		return repo.getCars();
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Car> getCar(@PathVariable String id){
+		Car requestedCar = repo.getCar(id);
+		if(requestedCar == null) return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(requestedCar);
+	}
+	
 }
