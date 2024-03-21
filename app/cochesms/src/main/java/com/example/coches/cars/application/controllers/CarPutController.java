b4397c00ee7c33aef565cc5ff905513853315420
AnderGI <agibarguen@gmail.com>
2024-03-21 14:22:26 +0100

    Put test added

---
+++ app/cochesms/src/main/java/com/example/coches/cars/application/controllers/CarPutController.java
@@ -0,0 +1,32 @@
+package com.example.coches.cars.application.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.coches.cars.application.convertCarToJson.CarToJsonConverter;
+import com.example.coches.cars.domain.car.Car;
+import com.example.coches.cars.domain.car.CarRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@RestController
+public class CarPutController {
+	@Autowired
+	private CarRepository repository;
+	@Autowired
+	private ObjectMapper mapper;
+	@PutMapping("/cars/{id}")
+	public ResponseEntity<ObjectNode> updateCar(@PathVariable String id,
+			@RequestBody Car car){
+	     Car updatedCar = repository.updateCar(car, id);
+	        if (updatedCar == null) {
+	            return ResponseEntity.notFound().build();
+	        }
+	        return ResponseEntity.ok(CarToJsonConverter.convert_car_to_json(updatedCar, mapper));
+	}
+	
+}
