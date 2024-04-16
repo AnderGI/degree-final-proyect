1b6539e4b62077dd5f81f1b9769eb67af86eaaff
AnderGI <75628843+AnderGI@users.noreply.github.com>
2024-04-16 09:26:18 +0200

    Update getCarById.ts
    Changed getCarById use case, implementing new repository (getCar as a type that is being exported, functional) and currying.

--- app-cliente/src/modules/cars/application/get/getCar/getCarById.ts
+++ app-cliente/src/modules/cars/application/get/getCar/getCarById.ts
@@ -5,3 +5,14 @@ import { CarRepository } from "../../../domain/car/CarRepository";
 export async function getCarById(repository: CarRepository, id: String) : Promise<Car> {
     return firstValueFrom(repository.getCar(id));
 }
+
+/*
+Aplicando currying...
+Lo que va a recibir es el CarId
+Antes de implementarlo ---> TESTING!!!!!
+export function getCarById(repository:getCar) {
+  return async function({id:CarId}):Promise<Car>{
+     return firstValueFrom(repository.getCar(id));
+  }
+}
+*/
