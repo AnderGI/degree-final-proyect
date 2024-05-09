a5ff08bab8c48c78b47bb668f667e7ceee337955
AnderGI <agibarguen@gmail.com>
2024-05-09 08:20:08 +0200

    GetAllCarsUnitTester moved from application layer to infrastructure and renamed GetAllCarsAcceptanceTester

--- app/cochesms/src/test/java/com/example/coches/cars/infraestructure/controllers/get/get_all_cars/GetAllCarsAcceptanceTester.java
+++ app/cochesms/src/test/java/com/example/coches/cars/infraestructure/controllers/get/get_all_cars/GetAllCarsAcceptanceTester.java
@@ -1,13 +1,15 @@
-package com.example.coches.cars.infraestructure.controllers.get;
+package com.example.coches.cars.infraestructure.controllers.get.get_all_cars;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +27,12 @@ import com.example.coches.cars.domain.car.CarPrice;
 import com.example.coches.cars.domain.car.CarRepository;
 import com.example.coches.cars.domain.car.CarTitle;
 import com.example.coches.cars.domain.car.CarUrl;
+import com.example.coches.cars.domain.convert_car_model_to_json_model.CarToJsonConverter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest
-public class GetAllCarBrandsAcceptanceTester {
+public class GetAllCarsAcceptanceTester {
+
 	@Autowired
 	private MockMvc mockMvc;
 	@Autowired
@@ -38,9 +42,25 @@ public class GetAllCarBrandsAcceptanceTester {
 	@MockBean
 	private CarRepository repository;
 	
+	// Casos : Que el array este vacio o que no lo este
+	
 	@Test
-	void it_should_retreive_all_car_brands_as_a_string_list() 
-	throws Exception{
+	void return_empty_array_if_empty_repository() throws Exception {
+		List<Car> expectedCars = new ArrayList<>();
+		
+		when(repository.getCars()).thenReturn(expectedCars);
+		
+		mockMvc.perform(get("/cars").accept(MediaType.APPLICATION_JSON))
+		.andDo(print())
+		.andExpectAll(
+				status().isOk(),
+				content().contentType(MediaType.APPLICATION_JSON),
+				content().json(mapper.writeValueAsString(expectedCars))
+		);
+	}
+
+	@Test
+	void return_a_list_of_products_if_repository_is_not_empty() throws Exception {
 		List<Car> expectedCars = Arrays.asList(
 				new Car(new CarId(UUID.randomUUID().toString()),new CarTitle("Chevrolet Camaro SS"), new CarDescription("Muscle car americano"),
 						new CarBrand("Chevrolet"), new CarPrice(55000.0),
@@ -52,19 +72,31 @@ public class GetAllCarBrandsAcceptanceTester {
 				new Car(new CarId(UUID.randomUUID().toString()),new CarTitle("Audi R8"), new CarDescription("Supercar de alta gama"), new CarBrand("Audi"),
 						new CarPrice(150000.0), new CarUrl("https://example.com/audi-r8.jpg"),
 						new CarUrl("https://example.com/audi-r8-listing")));
-		expectedCars.forEach(car -> repository.addCar(car));
-		
-		List<String> expectedBrands = expectedCars.stream().map(car -> car.getBrandValue()).toList();
-		
-		when(repository.getAllBrands()).thenReturn(expectedBrands);
-		
-		mockMvc.perform(get("/cars/brands")).andDo(print())
-		.andExpectAll(
-				status().isOk(),
-				content().contentType(MediaType.APPLICATION_JSON_VALUE),
-				content().json(mapper.writeValueAsString(expectedBrands))
-		);
-		
+		repository.addCar(expectedCars.get(0));
+		repository.addCar(expectedCars.get(1));
+		repository.addCar(expectedCars.get(2));
+
 	
+		// Del array de expectedCars lo pasamos a una mapeador
+		// Convierte a ObjectNode y luego al string 
+		// creamos una lista y luego su representacion como string
+		String cochesArrayToString = expectedCars.stream()
+				.map((Car car) -> CarToJsonConverter.convert(car, mapper).toString())
+				.collect(Collectors.toList())
+				.toString();
+		
+		when(repository.getCars()).thenReturn(expectedCars);
+
+	
+		mockMvc.perform(
+					get("/cars")
+					.accept(MediaType.APPLICATION_JSON)
+				)
+				.andDo(print())
+				.andExpectAll(
+						status().isOk(),
+						content().contentType(MediaType.APPLICATION_JSON),
+						content().json(cochesArrayToString)
+						);
 	}
 }
