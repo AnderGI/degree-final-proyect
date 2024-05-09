354fad602cba0bc5e228bf27542ed0e09b2a66a5
AnderGI <agibarguen@gmail.com>
2024-05-09 10:20:51 +0200

    SaveCarUnitTester tests added for happy and not happy paths

--- app/cochesms/src/test/java/com/example/coches/cars/application/add_car/AddCarUnitTester.java
+++ app/cochesms/src/test/java/com/example/coches/cars/application/add_car/AddCarUnitTester.java
@@ -1,5 +1,7 @@
 package com.example.coches.cars.application.add_car;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 import java.util.UUID;
@@ -17,25 +19,36 @@ import com.example.coches.cars.domain.car.CarTitle;
 import com.example.coches.cars.domain.car.CarUrl;
 import com.example.coches.cars.infraestructure.adapters.cars.MongoDBCarRepository;
 
-
 // Al ser tests unitarios (partimos desde la capa de aplicacion, subject under test)
 // hay que falsear la implementacion del repositorio
 // en este caso mediante mocks
 public class AddCarUnitTester {
 	@Test
-	void it_should_save_a_correct_car() {
-	    Car car =  new Car(new CarId(UUID.randomUUID().toString()), 
-	    		new CarTitle("ACTUALIZADO"), 
-	            new CarDescription("ACTUALIZADO"), 
-	            new CarBrand("BMW"), new CarPrice(70000.0), 
-	            new CarUrl("https://example.com/bmw-m3.jpg"), 
-	            new CarUrl("https://example.com/bmw-m3-listing"));
+	void it_should_save_a_correct_car() throws Exception {
+		Car car = new Car(new CarId(UUID.randomUUID().toString()), new CarTitle("ACTUALIZADO"),
+				new CarDescription("ACTUALIZADO"), new CarBrand("BMW"), new CarPrice(70000.0),
+				new CarUrl("https://example.com/bmw-m3.jpg"), new CarUrl("https://example.com/bmw-m3-listing"));
 		// Mock mongodbrepository
 		CarRepository mockMongoDbRepo = Mockito.mock(MongoDBCarRepository.class);
 		// Instanciar el caso de uso con el mock del repositorio
 		CarSaver saverUseCase = new CarSaver(mockMongoDbRepo);
-		// En este caso como es un metodo que devuelve void lo unico que 
-		// se testea es que no "pete"
-		//saverUseCase.save_car(car);
+		// En este caso como es un metodo que devuelve void lo unico que
+		// se testea es que no "pete" lanzando ninguna excepcción
+		saverUseCase.save_car(car);
+	}
+	
+	@Test
+	void it_should_not_save_an_incorrect_car() {
+		Car invalidCar = null;
+		// Mock mongodbrepository
+		CarRepository mockMongoDbRepo = Mockito.mock(MongoDBCarRepository.class);
+		// Instanciar el caso de uso con el mock del repositorio
+		CarSaver saverUseCase = new CarSaver(mockMongoDbRepo);
+		// testeare que la excepcion no es null porque va a lanzar una
+		try {
+			saverUseCase.save_car(invalidCar);
+		}catch (Exception e) {
+			assertNotNull(e);
+		}
 	}
 }
